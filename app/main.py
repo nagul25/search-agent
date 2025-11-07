5ec89f08984f83d219273636bbbe2b8530139272
nagul99 <nagululaganathan@gmail.com>
2025-11-07 02:10:57 -0800

    Added new port in main

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(
     version="1.0.0",
 )
 
-origins = ["http://localhost:3000", "http://localhost:5173"]
+origins = ["http://localhost:3000", "http://localhost:5173", "http://localhost:5174"]
 
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
